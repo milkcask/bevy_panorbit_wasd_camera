1f27dc967fab095c33a5d506a5487f0351487759
milkcask <1572863+milkcask@users.noreply.github.com>
2025-10-30 16:31:26 +0000

    Add yaw support

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -5,6 +5,7 @@ use crate::{ActiveCameraData, PanOrbitCamera};
 #[derive(Resource, Default, Debug)]
 pub struct KeyboardTracker {
     pub pan: Vec2,
+    pub yaw: f32,
 }
 
 pub fn keyboard_tracker(
@@ -25,23 +26,45 @@ pub fn keyboard_tracker(
 
     let mut pan = Vec2::ZERO;
 
-    if pan_orbit.forward_key.is_some() && key_input.pressed(pan_orbit.forward_key.unwrap()) {
-        pan.y -= 1.;
+    if let Some(key) = pan_orbit.forward_key {
+        if key_input.pressed(key) {
+            pan.y -= 1.;
+        }
     }
-    if pan_orbit.backward_key.is_some() && key_input.pressed(pan_orbit.backward_key.unwrap()) {
-        pan.y += 1.;
+    if let Some(key) = pan_orbit.backward_key {
+        if key_input.pressed(key) {
+            pan.y += 1.;
+        }
     }
-    if pan_orbit.left_key.is_some() && key_input.pressed(pan_orbit.left_key.unwrap()) {
-        pan.x += 1.;
+    if let Some(key) = pan_orbit.left_key {
+        if key_input.pressed(key) {
+            pan.x += 1.;
+        }
     }
-    if pan_orbit.right_key.is_some() && key_input.pressed(pan_orbit.right_key.unwrap()) {
-        pan.x -= 1.;
+    if let Some(key) = pan_orbit.right_key {
+        if key_input.pressed(key) {
+            pan.x -= 1.;
+        }
     }
 
     // normalize to avoid faster diagonal movement
     if pan.length_squared() > 1. {
         pan = pan.normalize();
     }
-
     camera_movement.pan = pan;
+
+    let mut yaw = 0.0;
+    
+    if let Some(key) = pan_orbit.counter_clockwise_key {
+        if key_input.pressed(key) {
+            yaw -= 1.0_f32.to_radians();
+        }
+    }
+    
+    if let Some(key) = pan_orbit.clockwise_key {
+        if key_input.pressed(key) {
+            yaw += 1.0_f32.to_radians();
+        }
+    }
+    camera_movement.yaw = yaw;
 }
